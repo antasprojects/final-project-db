976ed10c7cb410b6b8b70de47b787110f74f4fe9
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-08-28 08:56:18 +0100

    Added new database tables and inserted dummy data

--- final-project-db.sql
+++ final-project-db.sql
@@ -23,9 +23,9 @@ CREATE TABLE IF NOT EXISTS Users (
 
 -- Insert dummy data into Users table
 INSERT INTO Users (username, email, password_hash) VALUES
-('john_doe', 'john@example.com', '$2b$12$8RmTIuexL8qPRA8tht6V0e'),
-('jane_smith', 'jane@example.com', '$2b$12$ZVmNHg1w3J8HFoMHZmtIvK'),
-('alice_wonder', 'alice@example.com', '$2b$12$5Le5eP9mK9BhxMPl/EbIyO');
+('john_doe', 'john@gmail', '$2b$12$8RmTIuexL8qPRA8tht6V0e'),
+('jane_smith', 'jane@gmail.com', '$2b$12$ZVmNHg1w3J8HFoMHZmtIvK'),
+('alice_wonder', 'alice@egmail.com', '$2b$12$5Le5eP9mK9BhxMPl/EbIyO');
 
 -- Create the Users_Profile table to store user preferences and visited spots
 CREATE TABLE IF NOT EXISTS Users_Profile (
